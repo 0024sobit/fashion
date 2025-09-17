62fe2da9c2b951631e94101a240d323744c8cc55
0024sobit <0024sobitvft@gmail.com>
2025-09-17 09:15:40 +0530

    .

--- fashion/src/Pages/Placeorder.jsx
+++ fashion/src/Pages/Placeorder.jsx
@@ -15,7 +15,26 @@ const Placeorder = () => {
             <div className='flex gap-[70px] whitespace-nowrap mt-10 font2 text-[12px]'>     
                 <p >INFORMATION</p> 
                 <p className='text-gray-400 font-light'>SHIPPING</p>        
-                <p className='text-gray-400 font-light'>PAYMENT</p> 
+                <p className='text-gray-400 font-light'>PAYMENT</p>       
+            </div>
+            <div className='flex flex-col gap-2 mt-6'>
+              <h1 className='font2 font-semibold tracking-tight text-sm mb-1'>CONTACT INFO</h1>    
+              <input className='border border-gray-300 py-1.5 px-3.5 w-full' type="text" placeholder='Email'  />
+              <input className='border border-gray-300 py-1.5 px-3.5 w-full' type="text" placeholder='Phone' />  
+            </div>
+            <div className='flex flex-col gap-2 mt-8'>             
+              <h1 className='font2 font-semibold tracking-tight text-sm mb-1'>SHIPPING ADDRESS</h1>       
+              <div className='inline-flex gap-2'>  
+                <input className='border border-gray-300 py-1.5 px-3.5 w-full' type="text" placeholder='Frist Name'  />
+                <input className='border border-gray-300 py-1.5 px-3.5 w-full' type="text" placeholder='Last Name' />
+              </div>
+                <input className='border border-gray-300 py-1.5 px-3.5 w-full' type="text" placeholder='Country' />
+                <input className='border border-gray-300 py-1.5 px-3.5 w-full' type="text" placeholder='State / Region' />
+                <input className='border border-gray-300 py-1.5 px-3.5 w-full' type="text" placeholder='Address' />  
+                <div className='inline-flex gap-2'>    
+                <input className='border border-gray-300 py-1.5 px-3.5 w-full' type="text" placeholder='City'  />   
+                <input className='border border-gray-300 py-1.5 px-3.5 w-full' type="text" placeholder='Postal Code' />    
+              </div>
             </div>
           </div>   
         </div>   
